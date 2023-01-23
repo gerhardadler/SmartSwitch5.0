0318402436e0d4be09633b67c90f16ee38122696
gerhardadler <gerhardskartveit@gmail.com>
2023-01-23 13:44:12 +0100

    formated code and added venv to gitignore

--- power_calculation.py
+++ power_calculation.py
@@ -1,10 +1,8 @@
 from collections import UserList
-import copy
 import requests
 from dataclasses import dataclass
 from datetime import datetime
 import config
-
 
 
 @dataclass()
@@ -22,8 +20,8 @@ class PowerHourList(UserList):
     def __init__(self, power_datetime: datetime, area):
         response = requests.get(
             f"https://www.hvakosterstrommen.no/api/v1/prices/{power_datetime:%Y}/{power_datetime:%m}-{power_datetime:%d}_{area}.json")
-        
-        self.data: list = [] # this is the list data that can be accessed like a list
+
+        self.data: list = []  # this is the list data that can be accessed like a list
         for hour in response.json():
             self.data.append(PowerHour(
                 hour["NOK_per_kWh"],
@@ -31,9 +29,9 @@ class PowerHourList(UserList):
                 datetime.fromisoformat(hour["time_end"]),
             ))
 
-
     def add_use_power(self):
-        use_power_count: int = int(len(self.data) * config.USE_POWER_PERCENTAGE)
+        use_power_count: int = int(
+            len(self.data) * config.USE_POWER_PERCENTAGE)
         self.data.sort(key=lambda powerhour: powerhour.nok_kwh)
 
         for hour in self.data[:use_power_count]:
@@ -44,4 +42,6 @@ class PowerHourList(UserList):
 
 powerhours = PowerHourList(datetime.now(), "NO1")
 powerhours.add_use_power()
-print(powerhours)
+
+for hour in powerhours:
+    print(hour)
