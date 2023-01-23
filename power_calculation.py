90ae1431231ac8c5fece2b543120f1e61c9e3ea8
gerhardadler <gerhardskartveit@gmail.com>
2023-01-23 10:25:18 +0100

    Made power_calculation oop

--- power_calculation.py
+++ power_calculation.py
@@ -1,3 +1,4 @@
+from collections import UserList
 import copy
 import requests
 from dataclasses import dataclass
@@ -17,35 +18,30 @@ class PowerHour:
         return f"{self.nok_kwh} {self.use_power}"
 
 
-# class PowerHourList:
+class PowerHourList(UserList):
+    def __init__(self, power_datetime: datetime, area):
+        response = requests.get(
+            f"https://www.hvakosterstrommen.no/api/v1/prices/{power_datetime:%Y}/{power_datetime:%m}-{power_datetime:%d}_{area}.json")
+        
+        self.data: list = [] # this is the list data that can be accessed like a list
+        for hour in response.json():
+            self.data.append(PowerHour(
+                hour["NOK_per_kWh"],
+                datetime.fromisoformat(hour["time_start"]),
+                datetime.fromisoformat(hour["time_end"]),
+            ))
 
 
-def get_hvakosterstrommen(power_datetime: datetime, area) -> list[PowerHour]:
-    response = requests.get(
-        f"https://www.hvakosterstrommen.no/api/v1/prices/{power_datetime:%Y}/{power_datetime:%m}-{power_datetime:%d}_{area}.json")
-    output: list = []
-    for hour in response.json():
-        output.append(PowerHour(
-            hour["NOK_per_kWh"],
-            datetime.fromisoformat(hour["time_start"]),
-            datetime.fromisoformat(hour["time_end"]),
-        ))
-    return output
+    def add_use_power(self):
+        use_power_count: int = int(len(self.data) * config.USE_POWER_PERCENTAGE)
+        self.data.sort(key=lambda powerhour: powerhour.nok_kwh)
+
+        for hour in self.data[:use_power_count]:
+            hour.use_power = True
+
+        self.data.sort(key=lambda powerhour: powerhour.time_start)
 
 
-def add_use_power(powerhours: list[PowerHour]) -> list[PowerHour]:
-    use_power_count: int = int(len(powerhours) * config.USE_POWER_PERCENTAGE)
-    price_sorted_hours: list[PowerHour] = sorted(
-        copy.deepcopy(powerhours), key=lambda powerhour: powerhour.nok_kwh)
-
-    for hour in price_sorted_hours[:use_power_count]:
-        hour.use_power = True
-
-    time_sorted_hours: list[PowerHour] = sorted(
-        price_sorted_hours, key=lambda powerhour: powerhour.time_start)
-    return time_sorted_hours
-
-
-powerhours = get_hvakosterstrommen(datetime.now(), "NO1")
-powerhours = add_use_power(powerhours)
+powerhours = PowerHourList(datetime.now(), "NO1")
+powerhours.add_use_power()
 print(powerhours)
