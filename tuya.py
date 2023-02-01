d0a7142206b573007f0194cc916ed16bdd8ad496
gerhardadler <gerhardskartveit@gmail.com>
2023-02-01 11:27:21 +0100

    removed log from tuya

--- tuya.py
+++ tuya.py
@@ -1,14 +1,10 @@
 import logging
-from config import ENDPOINT, ACCESS_ID, ACCESS_KEY, USERNAME, PASSWORD, DEVICE_ID
-
 from tuya_iot import TuyaOpenAPI, TUYA_LOGGER
+from config import ENDPOINT, ACCESS_ID, ACCESS_KEY, USERNAME, PASSWORD, DEVICE_ID
 
 # Initialization of tuya openapi
 openapi = TuyaOpenAPI(ENDPOINT, ACCESS_ID, ACCESS_KEY)
 openapi.connect(USERNAME, PASSWORD, "86", 'smartlife')
-
-TUYA_LOGGER.setLevel(logging.INFO)
-TUYA_LOGGER.addHandler(logging.FileHandler('logs.log'))
 
 def get_status():
     result = openapi.get(f"/v1.0/iot-03/devices/{DEVICE_ID}/status")
